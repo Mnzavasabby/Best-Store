be1912bef2c0d559a4f4ab8f47a09041e9fd3c57
Mnzavasabby <rashidsabitina@gmail.com>
2025-01-20 01:22:04 -0800

    added the files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App(){
+  return(
+    <>
+      <h2>Welcome to the website</h2>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
