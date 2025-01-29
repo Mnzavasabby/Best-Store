76f04058903975324ff5eb0884ff2bdaa2883ee1
Mnzavasabby <rashidsabitina@gmail.com>
2025-01-29 12:55:56 -0800

    added table of productslist

--- src/pages/admin/products/ProductList.js
+++ src/pages/admin/products/ProductList.js
@@ -1,19 +1,40 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function ProductList(){
-    return(
+export default function ProductList() {
+    const [products, setProducts] = useState([]);
+
+    function getProduct() {
+        fetch('http://localhost:4000/products?_sort=id&_order=desc')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Failed to fetch products");
+            })
+            .then(data => {
+                setProducts(data);
+            })
+            .catch(error => {
+                alert('Unable to fetch data');
+            });
+    }
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    return (
         <div className="container my-4">
-            <h2 className="text-center mb-4">products</h2>
+            <h2 className="text-center mb-4">Products</h2>
 
             <div className="row mb-3">
                 <div className="col">
-                    <Link className="btn btn-primary me-1" href="admin/products/create" role="button">create product</Link>
-                    <button type="button" className="btn btn-outline-primary">Refresh</button>
-                </div>
-                <div className="col">
-                    
+                    <Link className="btn btn-primary me-1" to="/admin/products/create" role="button">Create Product</Link>
+                    <button type="button" className="btn btn-outline-primary" onClick={getProduct}>Refresh</button>
                 </div>
             </div>
+
             <table className="table">
                 <thead>
                     <tr>
@@ -27,7 +48,28 @@ export default function ProductList(){
                         <th>Action</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <tr key={index}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>{product.brand}</td>
+                            <td>{product.category}</td>
+                            <td>{product.price}$</td>
+                            <td>
+                                <img src={`http://localhost:4000/products/images/${product.imageFilename}`} width="100" alt="..." />
+                            </td>
+                            <td>{product.createdAt.slice(0, 10)}</td>
+                            <td style={{ width: "10px", whiteSpace: "nowrap" }}>
+                                <Link className="btn btn-primary btn-sm me-1" to={`/admin/products/edit/${product.id}`}>
+                                    Edit
+                                </Link>
+                                <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
-    )
+    );
 }
